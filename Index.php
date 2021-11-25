3466cc2496bdcd7292777deaf6dfd56e0d478970
EustachyL <57866931+EustachyL@users.noreply.github.com>
2021-11-25 15:39:29 +0100

    draft

---
+++ Index.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Główna strona ELMng</title>
+	<link rel="stylesheet" href="css.css">
+</head>
+<body>
+	<header>
+		
+	</header>
+	
+	<nav>
+		<div class="navdiv">
+		</div>
+		<div class="navdiv">
+		Nowy
+		</div>
+		<div class="navdiv">
+		Projekty
+		</div>
+		<div class="navdiv">
+		Repozytoria
+		</div>
+		<div class="navdiv">
+		</div>
+		<div class="navdiv">
+		</div>
+	</nav>
+	
+	<div class="underniv">
+	</div>
+	<section class="flexbox">
+	<div class="header">
+	</div>
+		<main>
+			<div class="container" id="container">
+			
+				<div class="frame" id="a">
+					<div class="tile" >
+						<div class="titleDiv">
+							Nazwa
+						</div>
+						<div class="infoDiv">
+							<p class="infoTitle">Subtitle</p>
+							<p class="infoContent"> Content</p>
+							
+						</div>
+					</div>
+				</div>
+				
+				
+				<div class="frame" id="b">
+					<div class="tile" >
+						<div class="titleDiv">
+							Nazwa
+						</div>
+						<div class="infoDiv">
+							<p class="infoTitle">Subtitle</p><p class="infoContent"> Content</p>
+							
+						</div>
+					</div>
+				</div>
+				
+				
+			</div>
+		</main>
+	</section>
+	<script src="script.js">
+	</script>
+
+</body>
+</html>
