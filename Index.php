49ae3471511341797deaa48c8de09d3fa6771064
EustachyL <57866931+EustachyL@users.noreply.github.com>
2021-12-07 22:10:11 +0100

    grid generation ready

--- Index.php
+++ Index.php
@@ -3,12 +3,81 @@
 	<meta charset="utf-8">
 	<title>Główna strona ELMng</title>
 	<link rel="stylesheet" href="css.css">
+
+
+
 </head>
 <body>
-	<header>
+	<header id="header">
 		
 	</header>
-	
+			<?php
+				$level=0;
+				$id=0;
+				function clear_level()
+				{
+					global $level;
+					$level=0;
+				}
+				function update_level()
+				{
+					global $level;
+					$level++;
+					
+				}
+				function clear_id()
+				{
+					global $id;
+					$id=1;
+				}
+				function update_id()
+				{
+					global $id;
+					$id++;
+					
+				}
+				function queryProjects()
+				{
+					global $level;
+					global $id;
+					$polaczenie = mysqli_connect('localhost', 'root','','mngsitedb');
+					$quarry = "SELECT * from projekty WHERE level=".$level." AND parent_id=".$id;
+					$sql = mysqli_query($polaczenie, $quarry);
+					$projects=array(array());					
+					while($wiersz = mysqli_fetch_array($sql))
+					{
+					$linearray=[
+					0=>$wiersz['id'],
+					1=>$wiersz['parent_id'],
+					2=>$wiersz['level'],
+					3=>$wiersz['pos_col'],
+					4=>$wiersz['pos_row'],
+					];
+						array_push($projects,$linearray);
+					}
+					$polaczenie -> close();
+					return $projects;			
+						
+						
+				}	
+				function return_lvl_max()		
+				{
+					global $level;
+					global $id;
+					$polaczenie = mysqli_connect('localhost', 'root','','mngsitedb');
+					$quarry = "SELECT max(pos_col)as max_c, max(pos_row) AS max_r from projekty WHERE level=".$level." AND parent_id=".$id;
+					$sql = mysqli_query($polaczenie, $quarry);	
+					$wiersz = mysqli_fetch_array($sql);
+					$dims=[
+						"cols" =>$wiersz['max_c'],
+						"rows" =>$wiersz['max_r'],
+						];		
+						$polaczenie -> close();
+					return $dims;
+				}				
+			?>	
+			
+
 	<nav>
 		<div class="navdiv">
 		</div>
@@ -33,40 +102,56 @@
 	<div class="header">
 	</div>
 		<main>
-			<div class="container" id="container">
+			<div class="container" id="container" style="color:white;">
 			
-				<div class="frame" id="a">
-					<div class="tile" >
-						<div class="titleDiv">
-							Nazwa
-						</div>
-						<div class="infoDiv">
-							<p class="infoTitle">Subtitle</p>
-							<p class="infoContent"> Content</p>
-							
-						</div>
-					</div>
-				</div>
+				<script type="text/javascript">
+					function set_id(id)
+					{
+						<?php clear_id();?>
+						for(;id>0;id--)
+						{
+						<?php update_id();?>
+						}
+					}
+					function render_projects()
+					{
+						
+						var projects=<?php echo json_encode (queryProjects());?>;
+					}
+					
+					
+					function generate_grid()
+					{						
+						//clear container
+						document.getElementById("container").innerHTML="";
+						//quary dimentions
+						var maxcol=(<?php $dims=return_lvl_max(); echo json_encode($dims['cols']);?>)*1;
+						var maxrow=(<?php echo json_encode($dims['rows']);?>)*1;
+						//generate frames
+						for(var i=0;i<=maxcol+1;i++)
+						{
+							for(var j=0;j<=maxrow+1;j++)
+							{
+								var string="<div class='frame' id='"+i+"X"+j+"' style='grid-column-start:"+i+"; grid-column-end:"+(i+1)+"; grid-row-start:"+j+"; grid-row-end:"+(j+1)+";'></div>";
+								
+								document.getElementById("container").innerHTML+=string;
+							}
+						}
+					}
 				
 				
-				<div class="frame" id="b">
-					<div class="tile" >
-						<div class="titleDiv">
-							Nazwa
-						</div>
-						<div class="infoDiv">
-							<p class="infoTitle">Subtitle</p><p class="infoContent"> Content</p>
-							
-						</div>
-					</div>
-				</div>
+					window.onload = function() {
+						generate_grid(0);
+						
+
+					};
+				</script>
 				
 				
 			</div>
 		</main>
 	</section>
-	<script src="script.js">
-	</script>
+
 
 </body>
 </html>
